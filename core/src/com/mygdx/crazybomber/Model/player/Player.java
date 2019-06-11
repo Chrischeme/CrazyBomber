67e7506795fe2c7fd2e7923eb6e67466eddde70d
Miles <milesjiang18@gmail.com>
2019-06-10 20:35:45 -0400

    added bomb stack and player/bomb constructors

--- core/src/com/mygdx/crazybomber/Model/player/Player.java
+++ core/src/com/mygdx/crazybomber/Model/player/Player.java
@@ -1,13 +1,19 @@
 package com.mygdx.crazybomber.model.player;
 
+import com.mygdx.crazybomber.model.bomb.Bomb;
+
+import java.util.Stack;
+
 public class Player {
     private boolean _isKnockedUp;
     private boolean _isAlive;
     private int _numBombs;
+    private int _rangeBombs;
     private double _speed;
     private boolean onItem;
     private double _xCoordinate;
     private double _yCoordinate;
+    private Stack<Bomb> _bombStack;
 
 
     public double getXCoordinate() {
@@ -30,7 +36,7 @@ public class Player {
         return _speed;
     }
 
-    public void setSpeed(int _speed) {
+    public void setSpeed(double _speed) {
         this._speed = _speed;
     }
 
@@ -69,6 +75,14 @@ public class Player {
     public void pickUpItem(int itemXCoordinate, int itemYCoordinate) {
     }
 
+    public int getRangeBombs() {
+        return _rangeBombs;
+    }
+
+    public void setRangeBombs(int _rangeBombs) {
+        this._rangeBombs = _rangeBombs;
+    }
+
     public void move(char direction) {
         switch (direction) {
             case 'W':
@@ -85,4 +99,27 @@ public class Player {
                 break;
         }
     }
+
+    public Bomb dropBomb() {
+        Bomb droppedBomb =_bombStack.pop();
+        droppedBomb.setRangeBombs(getRangeBombs());
+        droppedBomb.setXCoordinate((int)getXCoordinate());
+        droppedBomb.setYCoordinate((int)getYCoordinate());
+        return droppedBomb;
+    }
+
+    public Player(double playerSpawnXCoordinate, double playerSpawnYCoordinate) {
+        Stack<Bomb> bombStack = new Stack<Bomb>();
+        setIsAlive(false);
+        setIsKnockedUp(false);
+        setNumBombs(1);
+        setOnItem(false);
+        setSpeed(1.5);
+        setXCoordinate(playerSpawnXCoordinate);
+        setYCoordinate(playerSpawnYCoordinate);
+        Bomb bomb = new Bomb((int) getXCoordinate(), (int) getYCoordinate(), getRangeBombs());
+        _bombStack.push(bomb);
+    }
+
+
 }
