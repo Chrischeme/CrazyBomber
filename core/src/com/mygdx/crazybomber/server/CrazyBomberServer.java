59422a3ec23dccbf78a527f5004ee4f5e9503bf0
Chris <ckim@gmail.com>
2019-07-19 20:54:35 -0400

    Added some logic for switch

--- core/src/com/mygdx/crazybomber/server/CrazyBomberServer.java
+++ core/src/com/mygdx/crazybomber/server/CrazyBomberServer.java
@@ -2,6 +2,12 @@ package com.mygdx.crazybomber.server;
 
 import com.badlogic.gdx.Game;
 import com.mygdx.crazybomber.Player;
+import com.mygdx.crazybomber.model.block.EmptyBlock;
+import com.mygdx.crazybomber.model.bomb.Bomb;
+import com.mygdx.crazybomber.model.item.BombUp;
+import com.mygdx.crazybomber.model.item.Item;
+import com.mygdx.crazybomber.model.item.RangeUp;
+import com.mygdx.crazybomber.model.item.SpeedUp;
 
 import java.io.*;
 import java.net.ServerSocket;
@@ -102,29 +108,49 @@ public class CrazyBomberServer {
                     if (length > 0) {
                         in.readFully(data, 0, data.length);
                     }
+                    ByteBuffer wrapped = ByteBuffer.wrap(data);
                     byte switchByte = data[0];
                     System.out.println("Updating: " + switchByte);
+                    int xCoord, yCoord;
                     switch (switchByte) {
                         // not sure about the data about which player, we might be able to decipher that with the different sockets
                         case 1:
                             // On player coord change
                             // data should have which player + new coord + if a movement key is pressed down (WASD)
                             // update the player coord in player data + send to all other players
+                            player.setX((float)wrapped.getDouble(1));
+                            player.setY((float)wrapped.getDouble(9));
                             break;
                         case 2:
                             // On player placed bomb
                             // data should have coord of bomb.  IMPLEMENT LATER : have time the bomb was placed
                             // update bomb in bomb data + send to all other players => NOT SURE
+
                             break;
                         case 3:
                             // On block broken
                             // data should have coord of broken block.
                             // update the block in map data + send to all other players
+                            xCoord = wrapped.getInt(1);
+                            yCoord = wrapped.getInt(5);
+                            gameState.getMap().blockMatrix[xCoord][yCoord] = new EmptyBlock(xCoord, yCoord);
                             break;
                         case 4:
                             // On item dropped
                             // data should have type of item + coord of item
                             // update item in item data + send to all other players => NOT SURE
+                            xCoord = wrapped.getInt(1);
+                            yCoord = wrapped.getInt(5);
+                            Item item;
+                            switch (data[9]) {
+                                case 0:
+                                    item = new BombUp(xCoord, yCoord, data[10]);
+                                case 1:
+                                    item = new RangeUp(xCoord, yCoord, data[10]);
+                                default:
+                                    item = new SpeedUp(xCoord, yCoord, data[10]);
+                            }
+                            gameState.getMap().getItemArray().add(item);
                             break;
                         case 5:
                             // On item pickedup
@@ -137,14 +163,21 @@ public class CrazyBomberServer {
                             // send to all other players
                             break;
                         case 7:
-                            // On player connect
-                            // data should have player data
-                            // send to all other players
+                            // Currently do not have a case for this one
                             break;
                         case 8:
                             // on Bomb explodes
-                            // data should have player data
+                            // data should have player data // MAYBE should have bomb range
                             // send to all other players
+                            xCoord = wrapped.getInt(1);
+                            yCoord = wrapped.getInt(5);
+                            for (int i = 0; i < gameState.getMap().getActiveBombArray().size(); i++) {
+                                if (gameState.getMap().getActiveBombArray().get(i).getXCoordinate() == xCoord &&
+                                        gameState.getMap().getActiveBombArray().get(i).getYCoordinate() == yCoord) {
+                                    gameState.getMap().getActiveBombArray().remove(i);
+                                    break;
+                                }
+                            }
                             break;
                         default:
                             // Unspecified
